fa9390c8b0699e4c1fd268ddfd900752ae516c97
nmontorio98 <43783308+nmontorio98@users.noreply.github.com>
2019-06-24 22:56:20 +1000

    Part 6
    a lot done. Adding fighting components for both player and enemies. add death functions. add render functions to cope with things dying.

--- engine.py
+++ engine.py
@@ -1,10 +1,12 @@
 import tcod as libtcod
 from input_handlers import handle_keys
 from entity import Entity, get_blocking_entities_at_location
-from render_functions import clear_all, render_all
+from render_functions import clear_all, render_all, RenderOrder
 from map_objects.game_map import GameMap
 from fov_functions import initialise_fov, recompute_fov
 from game_states import GameStates
+from components.fighter import Fighter
+from death_functions import kill_monster, kill_player
 
 def main():
     screen_width = 80
@@ -30,8 +32,9 @@ def main():
         'light_wall': libtcod.Color(130,110,50),
         'light_ground': libtcod.Color(200,180,50)
         }
-
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks = True)
+    
+    fighter_component = Fighter(hp = 30, defense = 2, power = 5)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks = True, render_order = RenderOrder.ACTOR, fighter = fighter_component)
     entities = [player]
 
     libtcod.console_set_custom_font("arial10x10.png",libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -55,7 +58,7 @@ def main():
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
         libtcod.console_flush()
 
         fov_recompute = False
@@ -66,6 +69,8 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get("fullscreen")
 
+        player_turn_results = []
+
         if move and game_state == GameStates.PLAYERS_TURN:
             dx,dy = move
             destination_x = player.x + dx
@@ -75,7 +80,8 @@ def main():
                 target = get_blocking_entities_at_location(entities, destination_x, destination_y)
 
                 if target:
-                    print("You kick the "+ target.name + ' in the shins, much to its annoyance!')
+                    attack_results = player.fighter.attack(target)
+                    player_turn_results.extend(attack_results)
                 else:
                     player.move(dx, dy)
                     fov_recompute = True
@@ -87,11 +93,46 @@ def main():
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
+        for result in player_turn_results:
+            message = result.get('message')
+            dead_entity = result.get('dead')
+
+            if message:
+                print(message)
+
+            if dead_entity:
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity)
+                else:
+                    message = kill_monster(dead_entity)
+                print(message)
+        
+
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
-                if entity != player:
-                    print("The "+ entity.name + " ponders the meaning of its existence.")
-            game_state = GameStates.PLAYERS_TURN
+                if entity.ai:
+                    enemy_turn_results = entity.ai.take_turn(player, fov_map, game_map, entities)
+
+                    for result in enemy_turn_results:
+                        message = result.get('message')
+                        dead_entity = result.get('dead')
+
+                        if message:
+                            print(message)
+                        
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity)
+                            else:
+                                message = kill_monster(dead_entity)
+                            print(message)
+
+                            if game_state == GameStates.PLAYER_DEAD:
+                                break
+                    if game_state == GameStates.PLAYER_DEAD:
+                                break
+            else:
+                game_state = GameStates.PLAYERS_TURN
 
 if __name__ == '__main__':
     main()

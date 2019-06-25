6d7a942c16ea87e3075e4db49f652dd918cb9bcd
nmontorio98 <43783308+nmontorio98@users.noreply.github.com>
2019-06-25 16:54:24 +1000

    part 7
    added UI, shows health bar and in game messages. Added mouse over effects

--- engine.py
+++ engine.py
@@ -7,13 +7,22 @@ from fov_functions import initialise_fov, recompute_fov
 from game_states import GameStates
 from components.fighter import Fighter
 from death_functions import kill_monster, kill_player
+from game_messages import MessageLog
 
 def main():
     screen_width = 80
     screen_height = 50
 
+    bar_width = 20
+    panel_height = 7
+    panel_y = screen_height - panel_height
+
+    message_x = bar_width + 2
+    message_width = screen_width - bar_width - 2
+    message_height = panel_height - 1
+
     map_width = 80
-    map_height = 45
+    map_height = 43
     
     room_max_size = 10
     room_min_size = 6
@@ -42,10 +51,13 @@ def main():
     libtcod.console_init_root(screen_width,screen_height, 'libtcod tutorial revised', False)
     
     con = libtcod.console_new(screen_width,screen_height)
+    panel = libtcod.console_new(screen_width, panel_height)
 
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room)
     fov_map = initialise_fov(game_map)
+
+    message_log = MessageLog(message_x, message_width, message_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -53,12 +65,12 @@ def main():
     game_state = GameStates.PLAYERS_TURN
 
     while not libtcod.console_is_window_closed():
-        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
 
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-        render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, mouse, colors)
         libtcod.console_flush()
 
         fov_recompute = False
@@ -98,14 +110,14 @@ def main():
             dead_entity = result.get('dead')
 
             if message:
-                print(message)
+                message_log.add_message(message)
 
             if dead_entity:
                 if dead_entity == player:
                     message, game_state = kill_player(dead_entity)
                 else:
                     message = kill_monster(dead_entity)
-                print(message)
+                message_log.add_message(message)
         
 
         if game_state == GameStates.ENEMY_TURN:
@@ -118,14 +130,14 @@ def main():
                         dead_entity = result.get('dead')
 
                         if message:
-                            print(message)
+                            message_log.add_message(message)
                         
                         if dead_entity:
                             if dead_entity == player:
                                 message, game_state = kill_player(dead_entity)
                             else:
                                 message = kill_monster(dead_entity)
-                            print(message)
+                            message_log.add_message(message)
 
                             if game_state == GameStates.PLAYER_DEAD:
                                 break
